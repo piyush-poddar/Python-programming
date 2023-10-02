2db1a2c987c77890e0c549caee0aa96b70a9824b
Piyush Poddar <contactme.piyush@gmail.com>
2023-10-02 21:03:03 +0530

    Adding addition operator code

---
+++ arithmetic-operators.py
@@ -0,0 +1,6 @@
+""" 
+This python program is to demonstrate the use of basic arithmetic operators in python.
+"""
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+print(num1,'+',num2,'=',num1+num2)
