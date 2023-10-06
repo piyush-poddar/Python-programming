54e0d3b6dcd0401599a54c9d35edef0749e413c4
Piyush Poddar <contactme.piyush@gmail.com>
2023-10-07 04:05:27 +0530

    Added more operators

--- arithmetic-operators.py
+++ arithmetic-operators.py
@@ -4,3 +4,7 @@ This python program is to demonstrate the use of basic arithmetic operators in p
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 print("Sum of",num1,"and",num2,"is",num1+num2)
+print("Difference of",num1,"and",num2,"is",num1-num2)
+print("Product of",num1,"and",num2,"is",num1*num2)
+print("Division of",num1,"and",num2,"is",num1/num2)
+
